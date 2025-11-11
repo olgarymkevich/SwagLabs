1043bf6e2cc8edb50aae79c990792c8ef6b25282
olgarymkevich <olga_rymkevich@mail.ru>
2025-11-11 14:32:17 +0300

    Tests added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,27 +3,33 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import pages.LoginPage;
+import pages.*;
 
 import java.time.Duration;
 
 public class BaseTest {
-    WebDriver browser;
+
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
+    public WebDriver driver;
     LoginPage loginPage;
+    ProductsPage productsPage;
 
     @BeforeMethod
     public void setUp(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-        browser = new ChromeDriver(options);
-        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
-        loginPage = new LoginPage(browser);
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
     }
 
     @AfterMethod
     public void close(){
-        browser.quit();
+        driver.quit();
     }
 }
