68af9956d125440870e7dd4bf0f117018f79790a
olgarymkevich <olga_rymkevich@mail.ru>
2025-11-19 15:30:55 +0300

    Dependencies added; PropertyReader, User, UserFactory, config.properties added; cross_browser.xml file added; smoke_tests.xml file added.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,34 +1,47 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
 import pages.*;
+import utils.PropertyReader;
 
 import java.time.Duration;
 
 public class BaseTest {
 
-    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
     public WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
     CartPage cartPage;
+    String user;
+    String password;
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
+    public void setUp(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            options.addArguments("--guest");
+            //options.addArguments("headless");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver();
+            driver = new EdgeDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
+
+        user = PropertyReader.getProperty("swaglabs.user");
+        password = PropertyReader.getProperty("swaglabs.password");
     }
 
     @AfterMethod
