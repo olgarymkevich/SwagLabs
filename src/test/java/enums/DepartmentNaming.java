a2be19cb8ce783c14c42c6f67b5675668a7cc805
olgarymkevich <olga_rymkevich@mail.ru>
2025-11-25 17:35:14 +0300

    Made changes to DepartmentNaming

--- src/test/java/enums/DepartmentNaming.java
+++ src/test/java/enums/DepartmentNaming.java
@@ -1,7 +1,11 @@
 package enums;
 
 public enum DepartmentNaming {
-    PRODUCTS("Products");
+    PRODUCTS("Products"),
+    CART("Your Cart"),
+    CHECKOUT_YOUR_INFORMATION("Checkout: Your Information"),
+    CHECKOUT_OVERVIEW("Checkout: Overview"),
+    CHECKOUT_COMPLETE("Checkout: Complete!");
 
     private final String displayName;
 
